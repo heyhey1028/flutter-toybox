d9c4cdc7a79a4230b016e1dc27365a17d75f25ce
heyhey1028 <shoheiogawa1028@gmail.com>
2021-08-28 00:05:18 +0900

    add: document model

--- lib/screens/mark_down/model/document.dart
+++ lib/screens/mark_down/model/document.dart
@@ -1,1 +1,11 @@
-class Document {}
+import 'package:flutter/material.dart';
+
+class Document {
+  Document({
+    @required this.title,
+    this.content,
+  });
+
+  final String title;
+  final String content;
+}
