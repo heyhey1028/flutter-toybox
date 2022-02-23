7f9621188706008b0fb57d3b140adac1aa8930a6
heyhey1028 <shoheiogawa1028@gmail.com>
2022-02-23 22:07:05 +0900

    update audio screen

--- lib/screens/just_audio/just_audio_screen.dart
+++ lib/screens/just_audio/just_audio_screen.dart
@@ -1,9 +1,23 @@
 import 'package:audio_video_progress_bar/audio_video_progress_bar.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_toybox/screens/just_audio/just_audio_screen_controller.dart';
 import 'package:flutter_toybox/widgets/app_scaffold.dart';
+import 'package:provider/provider.dart';
 
 class JustAudioScreen extends StatelessWidget {
   const JustAudioScreen({Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return ChangeNotifierProvider(
+      create: (_) => AudioScreenController()..init(),
+      child: _JustAudioScreen(),
+    );
+  }
+}
+
+class _JustAudioScreen extends StatelessWidget {
+  const _JustAudioScreen({Key key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
@@ -25,14 +39,53 @@ class JustAudioScreen extends StatelessWidget {
           child: Column(
             children: [
               Spacer(),
-              ProgressBar(
-                progress: Duration.zero,
-                total: Duration.zero,
+              Selector(
+                selector:
+                    (BuildContext ctx, AudioScreenController controller) =>
+                        controller.progressBarState,
+                builder: (BuildContext ctx, ProgressBarState state, _) =>
+                    ProgressBar(
+                  progress: state.current,
+                  buffered: state.buffered,
+                  total: state.total,
+                  onSeek: (Duration position) =>
+                      context.read<AudioScreenController>().seek(position),
+                ),
               ),
-              IconButton(
-                onPressed: () {},
-                icon: Icon(Icons.play_arrow),
-                iconSize: 32.0,
+              Selector(
+                selector:
+                    (BuildContext ctx, AudioScreenController controller) =>
+                        controller.audioState,
+                builder: (BuildContext ctx, AudioState state, _) {
+                  switch (state) {
+                    case AudioState.loading:
+                      return SizedBox(
+                        height: 32,
+                        width: 32,
+                        child: CircularProgressIndicator(),
+                      );
+                    case AudioState.ready:
+                    case AudioState.paused:
+                      return IconButton(
+                        onPressed: () =>
+                            context.read<AudioScreenController>().play(),
+                        icon: Icon(Icons.play_arrow),
+                        iconSize: 32.0,
+                      );
+                    case AudioState.playing:
+                      return IconButton(
+                        onPressed: () =>
+                            context.read<AudioScreenController>().pause(),
+                        icon: Icon(Icons.pause),
+                        iconSize: 32.0,
+                      );
+                    default:
+                      return SizedBox(
+                        height: 32,
+                        width: 32,
+                      );
+                  }
+                },
               )
             ],
           ),
