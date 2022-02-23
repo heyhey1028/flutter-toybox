815d3d10beb7efb19fee02a4b63040835bfbed8c
heyhey1028 <shoheiogawa1028@gmail.com>
2022-02-23 22:05:23 +0900

    init service locator and add audio_handler

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,12 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_toybox/screens/services/service_locator.dart';
 import 'package:flutter_toybox/widgets/app_scaffold.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
+  await initServiceLocator();
   runApp(MyApp());
 }
 
