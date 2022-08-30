cbdef6b9b0b06dbda2b1ad66321b9860a43df259
heyhey1028 <shoheiogawa1028@gmail.com>
2022-08-30 21:45:25 +0900

    update podfile

--- samples/square_sample/lib/features/sample/sample_screen.dart
+++ samples/square_sample/lib/features/sample/sample_screen.dart
@@ -1,11 +1,15 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:square_sample/widgets/app_scaffold.dart';
 
 class SampleScreen extends ConsumerWidget {
   const SampleScreen({super.key});
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    return Container();
+    return AppScaffold(
+      color: Colors.grey,
+      body: Container(),
+    );
   }
 }
