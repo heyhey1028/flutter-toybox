36284f098f07da9f03713fe4398d2e33e1aa7f57
heyhey1028 <shoheiogawa1028@gmail.com>
2022-09-01 21:36:15 +0900

    feat: integrate firebase

--- samples/square_sample/lib/main.dart
+++ samples/square_sample/lib/main.dart
@@ -1,13 +1,13 @@
-// import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-// import './firebase_options.dart';
+import './firebase_options.dart';
 import './widgets/app_scaffold.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  // await Firebase.initializeApp(
-  //   options: DefaultFirebaseOptions.currentPlatform,
-  // );
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const MyApp());
 }
 
