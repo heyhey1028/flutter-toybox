94b91f081ce554d42a0f2b59ce1547eb9c8c0828
heyhey1028 <shoheiogawa1028@gmail.com>
2022-09-02 23:58:29 +0900

    feat: create chain with animated widget screen

---
+++ samples/animation_playground/lib/features/chain_animation/chain_with_animated_widget.dart
@@ -0,0 +1,165 @@
+import 'package:animation_playground/widgets/base_button.dart';
+import 'package:flutter/material.dart';
+
+class ChainWithAnimatedWidget extends StatefulWidget {
+  const ChainWithAnimatedWidget({super.key});
+
+  @override
+  State<ChainWithAnimatedWidget> createState() =>
+      _ChainWithAnimatedWidgetState();
+}
+
+// 1. define StatefulWidget with SingleTickerProviderStateMixin
+class _ChainWithAnimatedWidgetState extends State<ChainWithAnimatedWidget>
+    with SingleTickerProviderStateMixin {
+  // 2. define AnimationController & Tween & Animation
+  late AnimationController _controller;
+  late TweenSequence<Alignment> _tween;
+  late Animation<Alignment> _alignAnimation;
+
+  bool hasAppeared = false;
+
+  @override
+  void initState() {
+    // 3. initialize controller
+    _controller = AnimationController(
+      vsync: this,
+      duration: const Duration(milliseconds: 1500),
+    );
+    // 4. prepare TweenSequence
+    _tween = TweenSequence<Alignment>(
+      [
+        TweenSequenceItem(
+          tween: Tween(
+            begin: const Alignment(-1, 3),
+            end: Alignment.topLeft,
+          ).chain(
+            CurveTween(curve: Curves.fastLinearToSlowEaseIn),
+          ),
+          weight: 2,
+        ),
+        TweenSequenceItem(
+          tween: ConstantTween(Alignment.topLeft),
+          weight: 1,
+        ),
+        TweenSequenceItem(
+          tween: Tween(
+            begin: Alignment.topLeft,
+            end: Alignment.topRight,
+          ),
+          weight: 1,
+        ),
+        TweenSequenceItem(
+          tween: Tween(
+            begin: Alignment.topRight,
+            end: Alignment.bottomLeft,
+          ),
+          weight: 1,
+        ),
+        TweenSequenceItem(
+          tween: Tween(
+            begin: Alignment.bottomLeft,
+            end: Alignment.bottomRight,
+          ),
+          weight: 1,
+        ),
+        TweenSequenceItem(
+          tween: Tween(
+            begin: Alignment.bottomRight,
+            end: Alignment.topLeft,
+          ),
+          weight: 1,
+        ),
+        TweenSequenceItem(
+          tween: Tween(
+            begin: Alignment.topLeft,
+            end: Alignment.topRight,
+          ),
+          weight: 1,
+        ),
+        TweenSequenceItem(
+          tween: ConstantTween(Alignment.topRight),
+          weight: 0.5,
+        ),
+        TweenSequenceItem(
+          tween: Tween(
+            begin: Alignment.topRight,
+            end: Alignment.center,
+          ).chain(
+            CurveTween(curve: Curves.easeIn),
+          ),
+          weight: 2.5,
+        ),
+      ],
+    );
+    // 5. create Animation by AnimationController x Tween
+    _alignAnimation = _controller.drive(_tween);
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Stack(
+      children: [
+        AnimatedBuilder(
+          // 6. merging animations to be apply multiple effects to within AnimatedBuilder
+          animation: _alignAnimation,
+          builder: (context, _) {
+            return Align(
+              alignment: _alignAnimation.value,
+              child: Image.asset(
+                'assets/images/dash_bird_pencil.png',
+                width: 200,
+                height: 200,
+              ),
+            );
+          },
+        ),
+        Align(
+          alignment: Alignment.bottomCenter,
+          child: Padding(
+            padding: const EdgeInsets.only(
+              bottom: 60,
+              right: 40,
+              left: 40,
+            ),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                const Text(
+                  'With AnimatedBuilder: ',
+                  style: TextStyle(
+                    color: Colors.blue,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                BaseButton(
+                  onPressed: () => _onPressed(_controller),
+                  text: !hasAppeared ? 'IN' : 'OUT',
+                ),
+              ],
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+
+  void _onPressed(
+    AnimationController controller,
+  ) {
+    if (controller.status == AnimationStatus.completed) {
+      controller.reverse();
+      setState(() => hasAppeared = false);
+      return;
+    }
+    controller.forward();
+    setState(() => hasAppeared = true);
+  }
+}
