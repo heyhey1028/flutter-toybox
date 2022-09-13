1ceb1b62f837f6cc1ba3d4f60f89ca72f19456c0
heyhey1028 <shoheiogawa1028@gmail.com>
2022-09-13 23:07:46 +0900

    feat: add multiple effect staggered animation

--- samples/animation_playground/lib/features/staggered_animation/staggered_multiple_effect_animation.dart
+++ samples/animation_playground/lib/features/staggered_animation/staggered_multiple_effect_animation.dart
@@ -1,3 +1,5 @@
+import 'dart:math';
+
 import 'package:animation_playground/constants/dash_bird.dart';
 import 'package:flutter/material.dart';
 
@@ -24,7 +26,7 @@ class _StaggeredMultipleEffectAnimationSampleState
     // 3. initialize controller
     _controller = AnimationController(
       vsync: this,
-      duration: const Duration(milliseconds: 1500),
+      duration: const Duration(milliseconds: 1000),
     );
 
     super.initState();
@@ -63,69 +65,27 @@ class AnimatedDashBirds extends AnimatedWidget {
 
   @override
   Widget build(BuildContext context) {
-
-    final birdOneAnimation = Tween<Offset>(
-      begin: const Offset(-1000, 0),
-      end: Offset.zero,
-    ).chain(Tween()).animate(
-          CurvedAnimation(
-            parent: _controller,
-            curve: const Interval(
-              0,
-              0.3,
-              // curve: Curves.fastLinearToSlowEaseIn,
-            ),
-          ),
-        );
-    final birdTwoAnimation = Tween<Offset>(
-      begin: const Offset(-1000, 0),
-      end: Offset.zero,
+    final birdOneOffsetAnimation = Tween<Offset>(
+      begin: const Offset(-500, 0),
+      end: const Offset(500, 0),
     ).animate(
       CurvedAnimation(
         parent: _controller,
         curve: const Interval(
-          0.1,
-          0.4,
-          // curve: Curves.fastLinearToSlowEaseIn,
-        ),
-      ),
-    );
-    final birdThreeAnimation = Tween<Offset>(
-      begin: const Offset(-1000, 0),
-      end: Offset.zero,
-    ).animate(
-      CurvedAnimation(
-        parent: _controller,
-        curve: const Interval(
-          0.2,
-          0.5,
-          // curve: Curves.fastLinearToSlowEaseIn,
-        ),
-      ),
-    );
-    final birdFourAnimation = Tween<Offset>(
-      begin: const Offset(-1000, 0),
-      end: Offset.zero,
-    ).animate(
-      CurvedAnimation(
-        parent: _controller,
-        curve: const Interval(
-          0.3,
+          0,
           0.6,
-          // curve: Curves.fastLinearToSlowEaseIn,
         ),
       ),
     );
-    final birdFiveAnimation = Tween<Offset>(
-      begin: const Offset(-1000, 0),
-      end: Offset.zero,
+    final birdOneRotateAnimation = Tween<double>(
+      begin: 0,
+      end: pi * 8,
     ).animate(
       CurvedAnimation(
         parent: _controller,
         curve: const Interval(
-          0.4,
-          0.7,
-          // curve: Curves.fastLinearToSlowEaseIn,
+          0,
+          0.6,
         ),
       ),
     );
@@ -137,43 +97,58 @@ class AnimatedDashBirds extends AnimatedWidget {
         children: [
           const SizedBox(height: 120),
           Transform.translate(
-            offset: birdOneAnimation.value,
-            child: Image.asset(
-              DashBird.coffee.path,
-              width: 200,
-              height: 120,
+            offset: birdOneOffsetAnimation.value,
+            child: Transform.rotate(
+              angle: birdOneRotateAnimation.value,
+              child: Image.asset(
+                DashBird.coffee.path,
+                width: 200,
+                height: 120,
+              ),
             ),
           ),
           Transform.translate(
-            offset: birdTwoAnimation.value,
-            child: Image.asset(
-              DashBird.nightcap.path,
-              width: 200,
-              height: 120,
+            offset: _getOffsetAnimation(_controller, 0.1, 0.7).value,
+            child: Transform.rotate(
+              angle: _getRotateAnimation(_controller, 0.1, 0.7).value,
+              child: Image.asset(
+                DashBird.nightcap.path,
+                width: 200,
+                height: 120,
+              ),
             ),
           ),
           Transform.translate(
-            offset: birdThreeAnimation.value,
-            child: Image.asset(
-              DashBird.glasses.path,
-              width: 200,
-              height: 120,
+            offset: _getOffsetAnimation(_controller, 0.2, 0.8).value,
+            child: Transform.rotate(
+              angle: _getRotateAnimation(_controller, 0.2, 0.8).value,
+              child: Image.asset(
+                DashBird.glasses.path,
+                width: 200,
+                height: 120,
+              ),
             ),
           ),
           Transform.translate(
-            offset: birdFourAnimation.value,
-            child: Image.asset(
-              DashBird.rockstar.path,
-              width: 200,
-              height: 120,
+            offset: _getOffsetAnimation(_controller, 0.3, 0.9).value,
+            child: Transform.rotate(
+              angle: _getRotateAnimation(_controller, 0.3, 0.9).value,
+              child: Image.asset(
+                DashBird.rockstar.path,
+                width: 200,
+                height: 120,
+              ),
             ),
           ),
           Transform.translate(
-            offset: birdFiveAnimation.value,
-            child: Image.asset(
-              DashBird.superdash.path,
-              width: 200,
-              height: 120,
+            offset: _getOffsetAnimation(_controller, 0.4, 1).value,
+            child: Transform.rotate(
+              angle: _getRotateAnimation(_controller, 0.4, 1).value,
+              child: Image.asset(
+                DashBird.superdash.path,
+                width: 200,
+                height: 120,
+              ),
             ),
           ),
         ],
@@ -181,23 +156,42 @@ class AnimatedDashBirds extends AnimatedWidget {
     );
   }
 
-  // Animation<Offset> _getAnimation(
-  //   AnimationController controller,
-  //   double begin,
-  //   double end,
-  // ) {
-  //   return Tween<Offset>(
-  //     begin: const Offset(-1000, 0),
-  //     end: Offset.zero,
-  //   ).animate(
-  //     CurvedAnimation(
-  //       parent: controller,
-  //       curve: Interval(
-  //         begin,
-  //         end,
-  //         curve: Curves.fastLinearToSlowEaseIn,
-  //       ),
-  //     ),
-  //   );
-  // }
+  Animation<Offset> _getOffsetAnimation(
+    AnimationController controller,
+    double begin,
+    double end,
+  ) {
+    return Tween<Offset>(
+      begin: const Offset(-500, 0),
+      end: const Offset(500, 0),
+    ).animate(
+      CurvedAnimation(
+        parent: controller,
+        curve: Interval(
+          begin,
+          end,
+        ),
+      ),
+    );
+  }
+
+  Animation<double> _getRotateAnimation(
+    AnimationController controller,
+    double begin,
+    double end,
+  ) {
+    return Tween<double>(
+      begin: 0,
+      end: pi * 8,
+    ).animate(
+      CurvedAnimation(
+        parent: controller,
+        curve: Interval(
+          begin,
+          end,
+          // curve: Curves.fastLinearToSlowEaseIn,
+        ),
+      ),
+    );
+  }
 }
